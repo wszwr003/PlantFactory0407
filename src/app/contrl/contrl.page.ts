c2523f21b775a4d19890226d5a5da0362bb91af8
HongliangWang <whl7770@gmail.com>
2019-04-11 23:49:13 +0800

    new nodesUI

--- src/app/contrl/contrl.page.ts
+++ src/app/contrl/contrl.page.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contrl.page.scss'],
 })
 export class ContrlPage implements OnInit {
-
+  private buttons = [1,2,3,4,5,6,7,8,9,10]
+  private states = ["已开启","已开启","已开启","已开启","已开启","已开启","已关闭","已关闭","已关闭","已关闭"]
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,12 +8,17 @@ import { Component } from '@angular/core';
 export class HomePage {
   public url_contrl:string = '/contrl';
   public url_collect:string = '/collect';
+  public url_water:string = '/water';
   public url_alarm:string = '/alarm';
   public url_history:string = '/history';
-
+  num:number = 4;
 
   constructor() { }
 
   ngOnInit() {}
+
+  onClick() {
+
+  }
 
 }
